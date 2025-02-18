8d7c8e1676235eaeac965b3d74d69ca796c6fb24
rodrigoncalves <rodrigosg2000@gmail.com>
2025-02-17 23:33:15 -0300

    initial commit

---
+++ src/main/java/com/code/rental/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.code.rental.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs*/**")
+                .requestMatchers("/h2-console/**");
+    }
+}
